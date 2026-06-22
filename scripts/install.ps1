d6171fd7cd72b6da40a7aec4e5bbc1c1a11669d3
Paul Houghton <paulirotta@gmail.com>
2026-06-22 21:12:24 +0300

    chore(release): bump version to 0.13.1 (#303)

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -9,8 +9,8 @@
 #   - Internet access to GitHub releases
 #
 # Version indicator for build script check:
-# -Version '0.13.0'
-# Install-OneSkill -Version '0.13.0'
+# -Version '0.13.1'
+# Install-OneSkill -Version '0.13.1'
 #
 # Environment variables:
 #   AHMA_INSTALL_DIR     - Override install directory (default: $HOME\.local\bin)
